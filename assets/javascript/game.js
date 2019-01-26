3785f9b24aed48d229a8413a4706056f3e438fc0
KodyShaw <kodyshaw16@gmail.com>
2019-01-26 12:45:39 -0700

    merged

---
+++ assets/javascript/game.js
@@ -0,0 +1,84 @@
+var randomNumber;
+var score = 0;
+
+var wins = 0;
+var losses = 0;
+var gem1 = 0;
+var gem2 = 0;
+var gem3 = 0;
+var gem4 = 0;
+
+//Randomizes Selected Number and all 4 gems.
+function restart() {
+
+    randomNumber;
+    score = 0;
+    $('#yourScore').text(score);
+
+    gem1 = 0;
+    gem2 = 0;
+    gem3 = 0;
+    gem4 = 0;
+
+    randomNumber = Math.floor(Math.random() * 120);
+    $('#selectedNumber').text(randomNumber);
+    gem1 = Math.floor(Math.random() * 12 + 1);
+    console.log('gem1 = ' + gem1);
+    gem2 = Math.floor(Math.random() * 12 + 1);
+    console.log('gem2 = ' + gem2);
+    gem3 = Math.floor(Math.random() * 12 + 1);
+    console.log('gem3 = ' + gem3);
+    gem4 = Math.floor(Math.random() * 12 + 1);
+    console.log('gem4 = ' + gem4);
+}
+
+restart();
+
+//Displays Initial Random Number, Your Score, Wins, and Losses
+$('#yourScore').text(score);
+$('#wins').text(wins);
+$('#losses').text(losses);
+
+//Gem click functions
+$('#gem1').on('click', function () {
+    score = gem1 + score;
+    console.log('Score: ' + score);
+    $('#yourScore').text(score);
+    scoring();
+});
+
+$('#gem2').on('click', function () {
+    score = gem2 + score;
+    console.log('Score: ' + score);
+    $('#yourScore').text(score);
+    scoring();
+});
+
+$('#gem3').on('click', function () {
+    score = gem3 + score;
+    console.log('Score: ' + score);
+    $('#yourScore').text(score);
+    scoring();
+});
+
+$('#gem4').on('click', function () {
+    score = gem4 + score;
+    console.log('Score: ' + score);
+    $('#yourScore').text(score);
+    scoring();
+});
+
+//Scoring statements
+function scoring() {
+    if (randomNumber === score) {
+        wins++;
+        console.log('Wins: ' + wins);
+        $('#wins').text(wins);
+        restart();
+    } else if (score > randomNumber) {
+        losses++;
+        console.log('Losses: ' + losses);
+        $('#losses').text(losses);
+        restart();
+    }
+}
